da74efc6fee55c5a299c37128f892a711f7deb81
Firsh <firshee@gmail.com>
2024-01-23 19:42:34 +0000

    overengineered clock solution with useRef and querySelector

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -1,9 +1,19 @@
 'use client';
 import React from 'react';
 import format from 'date-fns/format';
+import Spinner from '../../../components/Spinner';
+
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+  //const [time, setTime] = React.useState(null);
+  const hasInitialValue = React.useRef(false);
+  let initialTime = new Date();
+  if (!hasInitialValue.current && typeof window !== "undefined") {
+    initialTime = document.querySelector('.clock').textContent;
+    hasInitialValue.current = true;
+    console.log("initial value established, this runs only once thanks to Ref");
+  }
+  const [time, setTime] = React.useState(initialTime);
 
   React.useEffect(() => {
     const intervalId = window.setInterval(() => {
@@ -14,9 +24,10 @@ function Clock() {
       window.clearInterval(intervalId);
     };
   }, []);
-
+  /* <p className="clock">{time !== null ? format(time, 'hh:mm:ss.S a') : <Spinner />}</p> */
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
+    <p className="clock">{typeof time !== "string" ? format(time, 'hh:mm:ss.S a') : time}</p>
+
   );
 }
 
